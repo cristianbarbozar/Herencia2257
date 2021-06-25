7b323f35061fe4fd9e077e40bd9e2e519961096b
cristianbarbozar <cristianbarboza398@aragon.unam.mx>
2021-06-24 22:35:44 -0500

    24/06/2021

---
+++ src/Ejerccio1/Persona.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejerccio1;
+
+/**
+ *
+ * @author CBR19
+ */
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    private int genero;
+    private float altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, int genero, float altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getGenero() {
+        return genero;
+    }
+
+    public void setGenero(int genero) {
+        this.genero = genero;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    
+    
+
+}
